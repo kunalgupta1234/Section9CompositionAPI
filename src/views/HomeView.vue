4f8bfd73f0a2564f63699d747ab36b8eccc28b63
KunalGupta <g.kunal9@gmail.com>
2024-03-29 19:15:10 +0530

    72-template refs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="home">
     home
-    <p>My name is {{ name }} and age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and age is {{ age }}</p>
     <button @click="handleClick">click</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "HomeView",
   setup() {
+    console.log(this);
+
+    const p = ref('hello');
+
     let name = "mario";
     let age = 30;
 
     const handleClick = () => {
-      console.log("you clicked me");
+      console.log(p, p.value);
+      p.value.classList.add('test')
+      p.value.textContent = "Helo ninjas"
     };
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, p };
   },
 };
 </script>
